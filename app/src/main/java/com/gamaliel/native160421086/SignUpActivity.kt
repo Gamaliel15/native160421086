730f70287a990b6d1c7d06cc6a438ecd9ad2b7e4
Gamaliel15 <s160421086@student.ubaya.ac.id>
2024-02-22 14:24:04 +0700

    sign up added

---
+++ app/src/main/java/com/gamaliel/native160421086/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.gamaliel.native160421086
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
