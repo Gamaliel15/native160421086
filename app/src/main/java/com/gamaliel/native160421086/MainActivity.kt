40995008f94892da2f144197487d4566edde078c
Gamaliel15 <s160421086@student.ubaya.ac.id>
2024-02-22 14:13:30 +0700

    init

---
+++ app/src/main/java/com/gamaliel/native160421086/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gamaliel.native160421086
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
